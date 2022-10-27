6f5032c4b200bc3eb500d9f39c99460482b82925
frasfras <65541080+frasfras@users.noreply.github.com>
2022-10-27 08:02:25 +0800

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -4,9 +4,7 @@
    <v-app id="inspire">
    <h3>Rep Alert
    </h3>
-   <iframe name ='PegaGadgetIfr' id ='PegaGadgetIfr' data-pega-iframe-gadgetname ='PegaGadget' data-pega-resizetype ='stretch' style= 'height: 400px; width: 100%;' src ='https://bsx4xypa.pegace.net/prweb/PRAuth/app/rep-alert_9674/!CreateAlert/?pzuiactionzzz=CXtpbn1qNWN2MHRBRFArcEt5dldFM1VFUythU3Fqb212UHd6MWZiUk5MSExhbG9sTkdPVFVvdUg2d1V3ckcyNnRKckJ6SGpuclJnNlJxQVV6NlhXKzR2SkN4MHNwUUtpTytleW1Ba2lqZjB5VFZlYWRteHFsSUVKWXJsTG84Y1BZRDFVUG9DbEdVUzJMV0JTeFJva2pIRGVnTkhvZUltR01QQ2c2cHhUem00NnJBMWlRck9WeFJzZVM0RXBjZytWemlQSGxRZ29OblFTemk1WUlibHhYdEtqbUpRaUZiR2VzK2J5QVhFNFh1R0pUMUV4OUFOdFVuc0lNSHM0cmxtOWI4RE9uV2xBQm52elhMQnJPRVkvZWFsblNGNXN0SDlTQjVHRnI3TXpyUVp4d2FoenJJTTd1ZUtEQkx3UERPR0hvSWlZOEVyTHZpQ2dLZjVUR1VmRWhxaE1WQXdtOUx2UjJXTHphTGdSMzh1VHpsUnpkbWlCbE1VVHJadG5NeDk2clVwNlg%3D*' ></iframe>
-<script src ='https://bsx4xypa.pegace.net/prweb/PRAuth/app/rep-alert_9674/?pzuiactionzzz=CXtpbn1jMitsM3M2SGg5NGV5bWVrU3VvSlNrSWxmV0FUL1BnS2hYSnBtK2N6d3B3RDR1L0hneUVpajRNYkx2WSs1aVFl*'></script>
- <!--  <PlanView /> -->
+  <!--  <PlanView /> -->
   </v-app>
   
   
